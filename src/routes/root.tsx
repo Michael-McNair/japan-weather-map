926647386fba86973b383120dc1285a3ff8a2482
michael mcnair <mikmcnair12@gmail.com>
2022-11-09 20:06:08 +0900

    make form

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,13 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import './root.css';
+
 export default function Root() {
   const ApiKey: string = '4b6b8969dc618e1bf2e2319466654a7d';
-  const lat: string = '35.689487';
-  const lon: string = '139.691711';
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
 
-  fetch(
-    `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${ApiKey}`
-  )
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+  // `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}$appid=${ApiKey}`
 
-  return <h1>hi</h1>;
+  return (
+    <div className="app">
+      <form className="location-name"></form>
+      <form className="lat-and-lon">
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1">latitude</label>
+          <input
+            type="text"
+            placeholder="0 ~ 180"
+            value={latitude}
+            onChange={(e: any) => setLatitude(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            longitude
+          </label>
+          <input
+            type="text"
+            placeholder="0 ~ 180"
+            value={longitude}
+            onChange={(e: any) => setLongitude(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
