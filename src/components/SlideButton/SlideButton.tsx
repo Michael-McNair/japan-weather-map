1c3f1b5efac0dc6d35be2687ac4df1a646862c4d
Michael McNair <michaeltsuyoshi@gmail.com>
2022-11-10 15:52:44 +0900

    add content to button component

--- src/components/SlideButton/SlideButton.tsx
+++ src/components/SlideButton/SlideButton.tsx
@@ -1,3 +1,7 @@
 export default function SlideButton(props: { onClick: any; content: string }) {
-  return <button className="slide" onClick={props.onClick}></button>;
+  return (
+    <button className="slide" onClick={props.onClick}>
+      {props.content}
+    </button>
+  );
 }
